1e6b29623723b7e3feb7a5d49f00d9bc35482950
navid <navid@nl>
2023-06-25 07:27:37 +0100

    fonts and shad init

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,24 @@
-import { Metadata } from "next";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
-  return <h1>Hi :)</h1>;
+  return <div>
+    <Card>
+      <CardHeader>
+        <CardTitle>بازی‌های من</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>بازی‌های من</p>
+      </CardFooter>
+    </Card></div>
+    ;
 }
-
-const APP_NAME = "App name";
-const APP_DESCRIPTION = "App Description";
-
-export const metadata: Metadata = {
-  title: "App",
-  description: APP_DESCRIPTION,
-  twitter: {
-    card: "summary_large_image",
-    creator: "@creator",
-    images: "https://example.com/og.png",
-  },
-  applicationName: APP_NAME,
-  appleWebApp: {
-    capable: true,
-    title: APP_NAME,
-    statusBarStyle: "default",
-  },
-  formatDetection: {
-    telephone: false,
-  },
-  themeColor: "#FFFFFF",
-  viewport:
-    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
-  manifest: "/manifest.json",
-  icons: [
-    { rel: "shortcut icon", url: "/favicon.ico" },
-  ],
-  keywords: ["nextjs", "pwa", "next-pwa"],
-};
-
